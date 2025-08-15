6cbcb30ccf1680113986cb0c3a74f521fb5acbd1
Dubb_smoken2 <danirut9986@gmail.com>
2025-08-15 16:06:26 +0700

    V.2: code ws01-04 but 05 not complete

---
+++ workshop03.py
@@ -0,0 +1,16 @@
+print('Hello')
+print("HI")
+print('''Hey''')
+print("""Hum""")
+
+print(48654) #/int
+print(15.1568) #float
+
+#boolean ตรรกะ มีสองค่า True หรือ False
+print(True)  # True
+print(False) # False
+
+#expression นิพจน์ คือ อะไรก็ตามที่มีการ process การทำงาน เช่น การคำนวณ การเปรียบเทียบ ตัวแปร ...
+print(10 + 20) 
+print(50 > 90) 
+print('Bangna' <= 'Bangpoo')
